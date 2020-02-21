17b1cd3a4cb219d843067b1d0462eb70b7919b63
lvan226 <lvan226@list.ru>
2020-02-21 21:29:54 +0800

    First day

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
+	$('.gallery__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: true,
+		dots: true
+	});
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,9 @@ function styles() {
 
 function scripts() {
 	return src([
-		// 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+		'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+		'node_modules/slick-carousel/slick/slick.min.js',
+		'app/js/slider.js',
 		'app/js/app.js' // app.js. Always at the end
 		])
 	.pipe(concat('app.min.js'))
